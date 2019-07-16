70f5d015de63fb6d7d24aa3214c3cb87422dc215
Mario <mario@die-davids.de>
2019-07-16 13:12:52 +0200

    proper name for extension and comments

--- modules/core/src/de/diedavids/cuba/ceusr/core/CustomerScheduledReportExtension.java
+++ modules/core/src/de/diedavids/cuba/ceusr/core/CustomerScheduledReportExtension.java
@@ -13,19 +13,32 @@ import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.*;
 
-@Component(CustomerScheduledReportParameterProvider.NAME)
-public class CustomerScheduledReportParameterProvider implements ScheduledReportExtension {
+@Component(CustomerScheduledReportExtension.NAME)
+public class CustomerScheduledReportExtension implements ScheduledReportExtension {
 
-    public static final String NAME = "ceusr_CustomerScheduledReportParameterProvider";
+    public static final String NAME = "ceusr_CustomerScheduledReportExtension";
+
     @Inject
     protected TimeSource timeSource;
 
 
+    /**
+     * this customer report extension checks for a particular scheduled report code to enrich
+     */
     @Override
     public boolean supports(ScheduledReport scheduledReport) {
+
         return scheduledReport.getCode().equals("big-customers");
     }
 
+    /**
+     * the report contains a parameter "totalTurnoverThreshold", that has to passed in
+     *
+     * the extension can configure the report with that parameter
+     *
+     * Currently the value is hard-coded in this extension class, but it can also be a result
+     * of a calculation on the current date e.g.
+     */
     @Override
     public Map<String, Object> provideParameters(ScheduledReport scheduledReport) {
         Map<String, Object> params = new HashMap<>();
@@ -33,11 +46,17 @@ public class CustomerScheduledReportParameterProvider implements ScheduledReport
         return params;
     }
 
+    /**
+     * the filename should contain the current date as part of the filename
+     */
     @Override
     public Optional<String> provideFilename(ScheduledReport scheduledReport, ReportTemplate reportTemplate) {
         return Optional.of("big-customers-" + today().toString());
     }
 
+    /**
+     * this report should be only executed at weekdays
+     */
     @Override
     public boolean shouldBeExecuted(ScheduledReport scheduledReport) {
 
@@ -62,6 +81,9 @@ public class CustomerScheduledReportParameterProvider implements ScheduledReport
         return now.toLocalDate();
     }
 
+    /**
+     * no particular report template is provided, therefore the default selection will be used
+     */
     @Override
     public Optional<ReportTemplate> provideReportTemplate(ScheduledReport scheduledReport, Report report) {
         return Optional.empty();
